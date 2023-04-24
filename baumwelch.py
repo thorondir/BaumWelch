f63ccf4e9df5ce2ebe77195364846092eae332a4
Ying Stokes <yingstokes@gmail.com>
2023-04-24 11:57:23 +0930

    finished implementation

--- baumwelch.py
+++ baumwelch.py
@@ -43,24 +43,34 @@ class HMM:
 
     def epsilon(self, alpha, beta, O):
         T = len(O)
-        epsilon = np.zeros(T-1, len(A), len(A))
+        epsilon = np.zeros((T-1, len(A), len(A)))
 
         for t in range(T-1):
             for i in range(len(A)):
                 for j in range(len(A)):
                     epsilon[t, i, j] = alpha[t,i]*A[i,j]*beta[t+1,j]*B[j,O[t+1]]
-            epsilon[t, i] /= np.sum(epsilon[t,i])
+            epsilon[t] /= np.sum(epsilon, axis=(1,2))[t]
+
+        return epsilon
 
     def update(self, O):
-        alpha = forward(O)
-        beta = backward(O)
-        gamma = gamma(alpha, beta)
-        epsilon = epsilon(alpha, beta, O)
+        T = len(O)
+
+        alpha = self.forward(O)
+        beta = self.backward(O)
+        gamma = self.gamma(alpha, beta)
+        epsilon = self.epsilon(alpha, beta, O)
 
         self.pi = gamma[0]
-        self.A = epsilon/gamma
+        self.A = np.sum(epsilon[0:T-1], axis=0)/np.sum(gamma[0:T-1], axis=0)
 
-        
+        barb = np.zeros(B.shape)
+        for j in range(k):
+            filter = [O[l]==j for l in range(len(O))]
+            barb[:,j] = np.sum(gamma[filter], axis=0)/np.sum(gamma, axis=0)
+
+        self.B = barb
+
 
 
     def baumWelch(self, O):
@@ -68,24 +78,31 @@ class HMM:
         beta = self.backward(O)
 
 
-#if __name__ == "__main__":
-n = 3
-k = 2
-A = np.array([[0.7, 0.2, 0.1],
-                [0.3, 0.5, 0.2],
-                [0.1, 0.3, 0.6]])
-B = np.array([[0.9, 0.1],
-                [0.2, 0.8],
-                [0.4, 0.6]])
-pi = np.array([0.6, 0.2, 0.2])
+if __name__ == "__main__":
+    n = 3
+    k = 2
+    A = np.array([[0.7, 0.2, 0.1],
+                    [0.3, 0.5, 0.2],
+                    [0.1, 0.3, 0.6]])
+    B = np.array([[0.9, 0.1],
+                    [0.2, 0.8],
+                    [0.4, 0.6]])
+    pi = np.array([0.6, 0.2, 0.2])
 
-hmm = HMM(n,k)
-hmm.A = A
-hmm.B = B
-hmm.pi = pi
+    hmm = HMM(n,k)
+    hmm.A = A
+    hmm.B = B
+    hmm.pi = pi
 
-O = np.array([0,1,0,1,0])
-print("forward")
-print(hmm.forward(O))
-print("backward")
-print(hmm.backward(O))
+    O = np.array([0,1,0,1,0])
+    print("forward")
+    alpha = hmm.forward(O)
+    print(alpha)
+    print("backward")
+    beta = hmm.backward(O)
+    print(beta)
+
+    hmm.update(O)
+    print(hmm.A)
+    print(hmm.B)
+    print(hmm.pi)
