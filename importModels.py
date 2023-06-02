1d611ba941157d1873efd439b5eaa48df888c574
Ying Stokes <yingstokes@gmail.com>
2023-06-02 18:38:16 +0930

    removed things irrelevant to 'release'

--- importModels.py
+++
@@ -1,8 +0,0 @@
-import jajapy
-
-models = dict()
-
-for i in range(1,18):
-    models[i] = jajapy.loadHMM('models/model' + str(i))
-for i in [32,64,128,256]:
-    models[i] = jajapy.loadHMM('models/model' + str(i))
--- importTraces.py
+++
@@ -1,6 +0,0 @@
-import csv
-
-traces = []
-
-with open('randtraces.csv', 'r') as f:
-    traces = list(csv.reader(f))
