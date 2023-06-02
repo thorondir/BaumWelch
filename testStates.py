c51a9c900943c3519979befbb0f9da33ad23a945
Ying Stokes <yingstokes@gmail.com>
2023-06-02 18:16:44 +0930

    final testStates.py changes

--- testStates.py
+++ testStates.py
@@ -1,11 +1,25 @@
 import jajapy, pm4py, random, pandas
+import sys
+from sys import argv
 baum_welch = jajapy.BW()
 
-logs = pm4py.read_xes('bpi_challenge_2013_incidents.xes')
-traces = [list(t[1]['lifecycle:transition']) for t in logs.groupby('case:concept:name')]
-random.shuffle(traces)
+if (argv[1] == 'help' or argv[1] == '-h' or argv[1] == '--help'):
+    print(len(argv))
+    print([a for a in argv])
+    print('usage:')
+    print('python testStates.py [event log] [output name] [optional argument if dataset uses BPI Challenge 2020 format]')
+    sys.exit()
 
-states = logs['lifecycle:transition'].unique()
+logs = pm4py.read_xes(argv[1])
+
+if (len(argv) > 3):
+    traces = [list(t[1]['concept:name']) for t in logs.groupby('case:id')]
+    states = logs['concept:name'].unique()
+else:
+    traces = [list(t[1]['lifecycle:transition']) for t in logs.groupby('case:concept:name')]
+    states = logs['lifecycle:transition'].unique()
+
+random.shuffle(traces)
 
 training_set = traces[:int((2*len(traces))/3)]
 test_set = jajapy.Set([t for t in traces[int((2*len(traces))/3):]])
@@ -15,7 +29,7 @@ models = dict()
 modelresults = dict()
 results = dict()
 
-for i in range(1,len(states)+5):
+for i in range(1,int(len(states)*1.5)+1):
     models[i] = jajapy.HMM_random(i, list(states), random_initial_state=True)
 
 for i in [32,64,128,256]:
@@ -23,14 +37,10 @@ for i in [32,64,128,256]:
 
 for i in models:
     print(i, "states")
-    if i > 128:
-        modelresults[i] = baum_welch.fit(training_set, models[i], max_it=100, return_data=True)[1]
-    else:
-        modelresults[i] = baum_welch.fit(training_set, models[i], epsilon=0, max_it=100, return_data=True)[1]
-
+    modelresults[i] = baum_welch.fit(training_set, models[i], epsilon=0, max_it=100, return_data=True)[1]
     ll = models[i].logLikelihood(test_set)
-    print("loglikelihood", ll)
+    print("logLikelihood", ll)
     results[i] = ll
 
-pandas.DataFrame.from_dict(results, orient='index').to_csv('loglikelihoods.csv', sep=',')
-pandas.DataFrame.from_dict(modelresults, orient='index').to_csv('models/modelresults')
+pandas.DataFrame.from_dict(results, orient='index').to_csv(argv[2] + 'LL.csv', sep=',')
+pandas.DataFrame.from_dict(modelresults, orient='index').to_csv(argv[2] + 'TrainingStats.csv')
